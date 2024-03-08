cc8d9dbedb60da9d3b19a47a4e61eb75b4e3ebd8
Emilio Landi <emilandi2000@hotmail.com>
2024-03-08 12:03:17 -0300

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,39 @@
+var rate;
+var sel;
+var url;
+var price;
+var aduana;
+var sitio = 1;
+
+console.log( "ready!" );
+
+// url = 'https://www.dolarsi.com/api/api.php?type=dolar';
+// url = 'https://dolarapi.com/v1/dolares';
+// 	fetch(url)
+//   		.then((response) => response.json())  		
+//   		.then((data) => result(data));	
+
+function result(data) {	
+    console.log(data);
+    // var resultado=data[0].casa.venta;
+    var resultado=data[0].venta;
+    console.log(resultado);    
+	var fixusd = parseFloat(resultado).toFixed(2);	
+    if (fixusd!=undefined) {
+        //chrome.browserAction.setBadgeText({text: 123}); // We have 10+ unread items.	        
+        //chrome.browserAction.setBadgeText({text: fixusd}); // We have 10+ unread items.	
+        //chrome.browserAction.setBadgeBackgroundColor({color:color});		
+        //chrome.browserAction.setTitle({title : msj + fixusd});
+        //chrome.browserAction.setTitle({title : msj });        
+    };		    
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.greeting === "hello")
+        sendResponse({farewell: "goodbye"});
+    }
+);
